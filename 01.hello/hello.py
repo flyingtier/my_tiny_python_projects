719ef0cf5b3ccc4500f65ee0b9cbf08605696f50
haru.keiko <haru.keiko@gmail.com>
2024-04-08 13:20:36 +0900

    add get_args function..

--- 01.hello/hello.py
+++ 01.hello/hello.py
@@ -3,12 +3,17 @@
 
 import argparse
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description='Say hello')
     #parser.add_argument('name', help='Name to greet')
     parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
-    args = parser.parse_args()
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
     print('Hello, ' + args.name + '!')
 
 if __name__ == '__main__':
