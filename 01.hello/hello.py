3e40e34a94f2e8bd2d68fb8c214884f42f710d28
haru.keiko <haru.keiko@gmail.com>
2024-04-08 13:40:07 +0900

    modify code to check python coding style.. for flake8 and pylint..

--- 01.hello/hello.py
+++ 01.hello/hello.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-# Purpose: Say hello
+"""
+Author: haru.keiko
+Purpose: Say hello
+"""
 
 import argparse
 
+
 def get_args():
+    """Get the command-line arguments"""
     parser = argparse.ArgumentParser(description='Say hello')
-    #parser.add_argument('name', help='Name to greet')
+    # parser.add_argument('name', help='Name to greet')
     parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
 
@@ -13,8 +18,10 @@ def get_args():
 
 
 def main():
+    """Go!!"""
     args = get_args()
     print('Hello, ' + args.name + '!')
+
 
 if __name__ == '__main__':
     main()
